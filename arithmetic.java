61a35065b734e766623f166c5bdc84405b44c6b8
RijishaJohnson <jrijisha@gmail.com>
2025-02-12 12:05:43 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        byte a= 127;
+        byte b= 3;
+       byte c=(byte)(a+b);
+       byte d=(byte)(a-b);
+       byte e=(byte)(a*b);
+       byte f=(byte)(a/b);
+        System.out.println("c="+c);
+         System.out.println("d="+d);
+          System.out.println("e="+e);
+           System.out.println("f="+f);
+    }
+}
---
+++ logical.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+      int a=10;
+      int b=23;
+      int c=40;
+      
+        System.out.println(a>b&&a>c);
+        System.out.println(b>c||c>a);
+        
+      
+    
+    }
+}
