ae3aa7e2e9e272ab0f790f3adf34a5cfa6d14bab
RijishaJohnson <jrijisha@gmail.com>
2025-02-13 11:57:39 +0530

    Create 123.java

---
+++ 123.java
@@ -0,0 +1,18 @@
+
+
+class Main {
+    public static void main(String[] args) {
+        int n=5;
+        int i,j;
+        int k=1;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(j+"");
+            } System.out.println(" ");
+            
+            
+        }
+    }
+}
