956f1a0f8361bb4aaa6794dd220d90ff6fdb7d2d
RijishaJohnson <jrijisha@gmail.com>
2025-02-24 15:03:38 +0530

    Create final.java

---
+++ final.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ abstract class Circle{
+final int pi=3.14;
+static double n=7;
+static double area;
+static void calc_area(){
+area=pi*r*r;
+}
+static public main(String s[])
+{
+calc_area();
+}
+}
